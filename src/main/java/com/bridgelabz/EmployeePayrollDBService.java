89985f320b61038032fe906b13e49aafb5a0b79b
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-27 22:49:31 +0530

    MODIFIED: add functionality to update data using preparedstatement

--- src/main/java/com/bridgelabz/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollDBService.java
@@ -1,10 +1,6 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -64,6 +60,18 @@ public class EmployeePayrollDBService {
         try (Connection connection = getConnection();) {
             Statement statement = connection.createStatement();
             return statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new DatabaseException("Error while executing the query", ExceptionType.UNABLE_TO_EXECUTE_QUERY);
+        }
+    }
+
+    public int updateEmployeeSalaryUsingPreparedStatement(String name, double salary) throws DatabaseException {
+        String query = String.format("UPDATE employee_payrolljdbc set salary = ? where name = ?");
+        try (Connection connection = getConnection();) {
+            PreparedStatement prepareStatement = connection.prepareStatement(query);
+            prepareStatement.setString(2, name);
+            prepareStatement.setDouble(1, salary);
+            return prepareStatement.executeUpdate();
         } catch (SQLException e) {
             throw new DatabaseException("Error while executing the query", ExceptionType.UNABLE_TO_EXECUTE_QUERY);
         }
