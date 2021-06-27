034f084b1e686804eceb6da1ed340393328a4003
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-27 16:36:30 +0530

    MODIFIED: add functionality to connect to database

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -7,5 +7,34 @@ import java.util.Enumeration;
 
 public class DBDemo {
 
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_servicejdbc";
+        String user = "root";
+        String password = "Sql@777fa";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Driver Missing!", e);
+        }
 
+        listDrivers();
+
+        try {
+            connection = DriverManager.getConnection(jdbcURL, user, password);
+            System.out.println("Connection successfully established!" + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while(driverList.hasMoreElements()) {
+            Driver driver = driverList.nextElement();
+            System.out.println("   "+driver.getClass().getName());
+        }
+
+    }
 }
