444aaf7a074b352e11b7c27990a4c5c6069a8522
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-27 19:56:47 +0530

    MODIFIED: add functionality to update employee salary in DB

--- src/main/java/com/bridgelabz/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollDBService.java
@@ -7,18 +7,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
-import com.bridgelabz.DatabaseException;
 import com.bridgelabz.DatabaseException.ExceptionType;
-
-
 
 public class EmployeePayrollDBService {
 
     public ArrayList<EmployeePayrollData> readEmployeeData() throws DatabaseException {
         ArrayList<EmployeePayrollData> list = new ArrayList<EmployeePayrollData>();
         String query = "SELECT * FROM employee_payrolljdbc";
-        try (Connection connection = getConnection();) {
+        try (Connection connection = getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
@@ -35,6 +33,12 @@ public class EmployeePayrollDBService {
         }
     }
 
+    /**
+     * @returns Connection Object
+     * @throws DatabaseException
+     *
+     */
+
     private Connection getConnection() throws DatabaseException {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_servicejdbc";
         String user = "root";
@@ -47,5 +51,32 @@ public class EmployeePayrollDBService {
             throw new DatabaseException("Unable to connect to the database", ExceptionType.UNABLE_TO_CONNECT);
         }
         return connection;
+    }
+
+    /**
+     * @param name
+     * @param salary
+     * @return execute update output
+     * @throws DatabaseException
+     */
+    public int updateEmployeeSalary(String name, double salary) throws DatabaseException {
+        String query = String.format("UPDATE employee_payrolljdbc set salary = %.2f where name = '%s'", salary, name);
+        try (Connection connection = getConnection();) {
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new DatabaseException("Error while executing the query", ExceptionType.UNABLE_TO_EXECUTE_QUERY);
+        }
+    }
+
+    /**
+     * @param name
+     * @return list of employee data with given name
+     * @throws DatabaseException
+     */
+    public ArrayList<EmployeePayrollData> getEmployeeData(String name) throws DatabaseException {
+        return readEmployeeData().stream()
+                .filter(employeePayrollData -> employeePayrollData.getEmpName().equals(name))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -20,6 +20,40 @@ public class EmployeePayrollService {
         return employeePayrollDataList;
     }
 
+    /**
+     * @param name
+     * @param salary
+     * @throws DatabaseException
+     * To Update Employee Salary
+     */
+    public void updateEmployeeSalary(String name, double salary) throws DatabaseException {
+        int result = new EmployeePayrollDBService().updateEmployeeSalary(name, salary);
+        if(result == 0) return;
+        EmployeePayrollData employeePayrollData = getEmployeeData(name);
+        if(employeePayrollData != null) employeePayrollData.setSalary(salary);
+    }
+
+    /**
+     * @param name
+     * @return T/F whether Payroll is in sync with DB or not
+     * @throws DatabaseException
+     */
+    public boolean isEmployeePayrollInSyncWithDB(String name) throws DatabaseException {
+        ArrayList<EmployeePayrollData> list = new EmployeePayrollDBService().getEmployeeData(name);
+        return list.get(0).equals(getEmployeeData(name));
+    }
+
+    /**
+     * @param name
+     * @returns Employee Payroll Data Object with given employee name
+     */
+    private EmployeePayrollData getEmployeeData(String name) {
+        return employeePayrollDataList.stream()
+                .filter(employeePayrollData -> employeePayrollData.getEmpName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
     public long employeeDataSize() {
         return employeePayrollDataList.size();
     }
